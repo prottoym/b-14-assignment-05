75fe9090e9c941ee46bf32d761d1463b05c5bd45
prottoym <nilantomodak7768869@gmail.com>
2026-09-11 00:04:09 +0600

    Nav

---
+++ Dev-Stack/src/components/Nav.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Logo from '../assets/logo-text.png';
+
+const Nav = () => {
+    return (
+
+        // <nav className="mx-auto h-[80px] w-[1280px] border-b border-gray-200 bg-white">
+        <nav className="w-full h-[80px] border-b border-gray-200 bg-white">
+
+            <div className="flex h-full items-center justify-between px-6">
+
+                {/* Logo */}
+                <img src={Logo} alt="Dev Stack" className="w-[130px] h-auto" />
+
+                {/* Middle items part */}
+                <ul className="flex items-center gap-8 text-[14px] font-medium text-gray-500">
+                    <li className="text-pink-500">Home</li>
+                    <li>Technologies</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+                {/* buttons part */}
+                <div className="flex items-center gap-5">
+                    <button className="text-[14px] font-medium text-gray-600">
+                        Sign In
+                    </button>
+
+                    <button className="rounded-full bg-pink-600 px-6 py-3 text-[14px] font-medium text-white">
+                        Sign Up
+                    </button>
+                </div>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
