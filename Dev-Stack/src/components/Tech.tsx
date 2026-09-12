d697986cb21faa2f8d2053456c18eb0a467df8e3
prottoym <nilantomodak7768869@gmail.com>
2026-09-12 13:22:27 +0600

    Tech

--- Dev-Stack/src/components/Tech.tsx
+++ Dev-Stack/src/components/Tech.tsx
@@ -235,10 +235,12 @@ export default function Tech() {
           </div>
         </div>
       </div>
+
+      {/* Section end divider */}
+      <div className="border-b border-gray-200 bg-white mt-10"></div>
     </div>
   );
 }
-
 
 
 // import React from 'react';
